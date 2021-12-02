01f6a448074428691b2d93a1d434aea1a6d37e15
kevinguyodo <kevin.guyodo@ynov.com>
2021-12-02 16:12:27 +0100

    update

--- Data/Piece.cs
+++ Data/Piece.cs
@@ -5,30 +5,14 @@ namespace Project_Tetris_cs.Data {
 
     public class Tetris {
 
-        // public object getRandomPiece() {
-        //     allPiece = new object[7] {line, letterJ, letterL, block, letterS, letterT, letterZ};
-        //     Random randomPiece = new Random();
-        //     int randomPieceIndex = randomPiece.Next(0, allPiece.Length);
-        //     Console.WriteLine(allPiece[randomPieceIndex]);
-        //     return allPiece[randomPieceIndex];
-        // }
-
-        // public int[,] getCoordinates() {
-        //     object pieceToGetCoordinate = getRandomPiece();
-        //     int[,] pieceCoordinate = pieceToGetCoordinate.getPieceCoordinates();
-        //     Console.WriteLine(pieceCoordinate);
-        //     return pieceCoordinate;
-
-        // }
-
         public object[] allPiece {get;set;}
-        Barre line = new Barre();
-        Gamma letterJ = new Gamma();
-        LetterL letterL = new LetterL();
-        Carre block = new Carre();
-        ReverseBiais letterS = new ReverseBiais();
-        LetterT letterT = new LetterT();
-        Biais letterZ = new Biais();
+        public Barre line = new Barre();
+        public Gamma letterJ = new Gamma();
+        public LetterL letterL = new LetterL();
+        public Carre block = new Carre();
+        public ReverseBiais letterS = new ReverseBiais();
+        public LetterT letterT = new LetterT();
+        public Biais letterZ = new Biais();
         public Tetris() {
             allPiece = new object[7] {line, letterJ, letterL, block, letterS, letterT, letterZ};
             // foreach(int i in line.shape) {
--- Data/Piece/AllShape.cs
+++ Data/Piece/AllShape.cs
@@ -2,9 +2,6 @@ using System;
 namespace Project_Tetris_cs.Data.Piece {
     public class AllShape {
         public int[,] shape {get; set;}
-        public int[,] getShapeCoordinates() {
-            return shape;
-        }
 
         public AllShape() {
         }
