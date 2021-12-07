cc996ecea0c118e1a0e25af7dfa58155289742e4
kevinguyodo <kevin.guyodo@ynov.com>
2021-12-07 13:54:29 +0100

    update

--- Data/Piece/AllShape.cs
+++ Data/Piece/AllShape.cs
@@ -18,19 +18,14 @@ namespace Project_Tetris_cs.Data.Piece {
         // }
 
         public void movePieceDown() {
-            // bool result = false;
             // Boucle en arrière pour conditions après car shape dans l'ordre décroissant
-            Console.WriteLine("Hello World");
             for (int index = this.shape.Length-1; index >= 0; index--) {
                 if (this.shape[index] >= 10 && this.shape[index] <= 200) {
                     this.shape[index] -= 10;
                 } else {
                     break;
-                    // Console.WriteLine("Ho shit!");
                 }          
             }
-            // return result;
-            
         }
     }
 }
