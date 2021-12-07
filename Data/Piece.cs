d8adee6a0e9e2db5a7d9cd637331e358859f6f0b
kevinguyodo <kevin.guyodo@ynov.com>
2021-12-07 13:26:41 +0100

    update movePieceInGrid

--- Data/Piece.cs
+++ Data/Piece.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Timers;
 using Project_Tetris_cs.Data.Piece;
 
 namespace Project_Tetris_cs.Data {
@@ -13,6 +14,9 @@ namespace Project_Tetris_cs.Data {
         public ReverseBiais letterS = new ReverseBiais();
         public LetterT letterT = new LetterT();
         public Biais letterZ = new Biais();
+
+        // private Timer timerToMoveShape = new Timer();
+
         public Tetris() {
             //     new int[,] {line.shape},
             //     new int[][,] {letterJ.shape};
@@ -25,8 +29,24 @@ namespace Project_Tetris_cs.Data {
         }
 
         public void getRandomPiece() {
-            var arrayOfShape = new [] {line, letterJ, letterL, block, letterS, letterT, letterZ};
+            var arrayOfShape = new AllShape[] {line, letterJ, letterL, block, letterS, letterT, letterZ};
         }
+
+        // public void movePieceDownTimer() {
+        //     timerToMoveShape.Interval = 1000;
+        //     timerToMoveShape.Elapsed +=  new ElapsedEventHandler(movePieceDown);
+        //     timerToMoveShape.AutoReset = true;
+        //     timerToMoveShape.Enabled = true;
+        // }
+
+        // public void movePieceDown(object source, ElapsedEventArgs e) {
+        //     // Boucle en arrière pour conditions après car shape dans l'ordre décroissant
+        //     for (int index = block.shape.Length; index >= 0; index--) {
+        //         if (block.shape[index] >= 0) {
+        //             block.shape[index]--;
+        //         } 
+        //     }
+        // }
         
     }
 }
--- Data/Piece/AllShape.cs
+++ Data/Piece/AllShape.cs
@@ -4,7 +4,27 @@ namespace Project_Tetris_cs.Data.Piece {
     public class AllShape {
         public int[] shape {get; set;}
 
+        private Timer timerToMoveShape = new Timer();
+
 
         public AllShape() {}
+
+        // public void movePieceDownTimer() {
+        //     timerToMoveShape.Interval = 1000;
+        //     timerToMoveShape.Elapsed +=  new ElapsedEventHandler(movePieceDown);
+        //     timerToMoveShape.AutoReset = true;
+        //     timerToMoveShape.Enabled = true;
+        //     timerToMoveShape.Start();
+        // }
+
+        public void movePieceDown() {
+            // Boucle en arrière pour conditions après car shape dans l'ordre décroissant
+            for (int index = shape.Length; index >= 0; index--) {
+                if (shape[index] >= 0) {
+                    shape[index] = shape[index] - 10;
+                }
+            }
+            Console.WriteLine("Hello World");
+        }
     }
 }
